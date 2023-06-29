dc7be0a779dcb7bacdd3767c4ce4ff4479f279c3
Mykhailo515 <mikhail.chursinov@gmail.com>
2023-06-29 20:21:59 +0300

    Init commit

--- src/homeworks/mihail_chursinov/hw_06_23/hw_20_06_23/task2/Test.java
+++ src/homeworks/mihail_chursinov/hw_06_23/hw_20_06_23/task2/Test.java
@@ -1,7 +1,7 @@
 package homeworks.mihail_chursinov.hw_06_23.hw_20_06_23.task2;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SyntaxStudentException {
         Main main = new Main();
         main.checkGrade(99);
     }
