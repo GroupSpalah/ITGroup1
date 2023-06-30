caeed5c0459ec75469cfdc1993ea4226fea788f5
Mykhailo515 <mikhail.chursinov@gmail.com>
2023-06-30 19:08:32 +0300

    Init commit

--- src/homeworks/mihail_chursinov/hw_06_23/hw_20_06_23/task2/Main.java
+++ src/homeworks/mihail_chursinov/hw_06_23/hw_20_06_23/task2/Main.java
@@ -8,15 +8,15 @@ package homeworks.mihail_chursinov.hw_06_23.hw_20_06_23.task2;
 public class Main {
     private int grade;
 
-    public void checkGrade(int grade) {
-        try {
-            if (grade > 90) {
-                throw new SyntaxStudentException("You are an exceptionally awesome student!");
-            } else if (grade < 90) {
-                System.out.println("You are a great student!");
-            }
-        } catch (SyntaxStudentException e) {
-            System.out.println(e.getMessage());
+
+    public void checkGrade(int grade) throws SyntaxStudentException {
+
+        if (grade > 90) {
+            throw new SyntaxStudentException("You are an exceptionally awesome student!");
+        } else if (grade < 90) {
+            System.out.println("You are a great student!");
         }
     }
+
+
 }
--- test/mihail_chursinov/hw_27_06_23/TestException.java
+++ test/mihail_chursinov/hw_27_06_23/TestException.java
@@ -2,12 +2,26 @@ package mihail_chursinov.hw_27_06_23;
 
 import homeworks.mihail_chursinov.hw_06_23.hw_20_06_23.task2.Main;
 import homeworks.mihail_chursinov.hw_06_23.hw_20_06_23.task2.SyntaxStudentException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TestException {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
     @Test(expected = SyntaxStudentException.class)
-    public void shouldCheckGrade() {
+    public void shouldCheckGrade() throws SyntaxStudentException {
         Main main = new Main();
         main.checkGrade(95);
+    }
+    @Test
+    public void shouldCheckGradeUsingRule() throws SyntaxStudentException {
+        Main main = new Main();
+
+        expectedException.expect(SyntaxStudentException.class);
+        expectedException.expectMessage("You are an exceptionally awesome student!");
+        main.checkGrade(95);
+
+
     }
 }
