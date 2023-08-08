d24601100c8a661ef431a73a0fcd37b8f219513c
anton gvozdenko <antongvozdenko1994@gmail.com>
2023-08-08 15:52:06 +0300

    add homework

--- src/homeworks/anton_gvozdenko/hw_28_07_23/Main.java
+++ src/homeworks/anton_gvozdenko/hw_28_07_23/Main.java
@@ -5,15 +5,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Translator translator = new Translator();
-translator.addNewWords("rus_eng","Друг","Friend");
-translator.addNewWords("rus_de","Вечер","Abend");
-translator.addNewLanguage("de_rus");
-translator.addNewLanguage("eng_rus");
-translator.addNewWords("eng_rus","Car","Машина");
-translator.addNewLanguage("spain_rus");
-translator.addNewWords("de_rus","Tak","Утро");
+        translator.addNewWords("rus_eng", "Друг", "Friend");
+        translator.addNewWords("rus_de", "Вечер", "Abend");
+        translator.addNewLanguage("de_rus");
+        translator.addNewWords("eng_rus", "Car", "Машина");
+        translator.addNewWords("rus_eng", "Машина", "Car");
+        translator.addNewWords("eng_rus", "Friend", "Друг");
+        translator.addNewLanguage("spain_rus");
+        translator.addNewWords("de_rus", "Tak", "Утро");
 
-        translator.isTranslationExist("Car", "eng_rus");
+        translator.translateSentence("Машина Утро Друг", "rus_eng");
+        translator.detectLanguage("Tak");
 
     }
 }
--- src/homeworks/anton_gvozdenko/hw_28_07_23/Translator.java
+++ src/homeworks/anton_gvozdenko/hw_28_07_23/Translator.java
@@ -45,33 +45,61 @@ public class Translator {
 
         Map<String, String> languageMap = map.get(language);
 
-        if (languageMap != null) {
+        if (languageMap == null) {
             languageMap = new HashMap<>();
             map.put(language, languageMap);
         }
         languageMap.put(wordIn, wordOut);
 
-        System.out.println(languageMap);
+
     }
 
     public void addNewLanguage(String language) {
         Map<String, String> stringMap = map.get(language);
-        if (stringMap == null) {
+        if (!map.containsKey(stringMap)) {
             stringMap = new HashMap<>();
             map.put(language, stringMap);
+            if (map.containsKey(stringMap)) {
+                System.out.println("Language doesn't add");
+            }
         }
     }
-
-
     public boolean isTranslationExist(String language, String word) {
         if (word != null) {
             Map<String, String> translation = map.get(language);
             return translation.containsKey(word);
         }
-
         return false;
     }
+    public void translateSentence(String sentence, String language) {
+        String[] words = sentence.split("\\s+");
+        StringBuilder translatedSentence = new StringBuilder();
+
+        for (String word : words) {
+            if (isTranslationExist(language, word)) {
+                String translatedWord = map.get(language).get(word);
+                translatedSentence.append(translatedWord).append(" ");
+            } else {
+                System.out.println("Translation not available for sentence: " + sentence);
+                return;
+            }
+        }
+        System.out.println(translatedSentence);
+
+    }
+
+    public void detectLanguage(String word) {
+        for (Map.Entry<String, Map<String, String>> entry : map.entrySet()) {
+            if (entry.getValue().containsKey(word)) {
+                String key = entry.getKey();
+                String[] result = key.split("_");
+                System.out.println(result[0]);
+            }
+        }
+    }
 }
+
+
 
 
 
