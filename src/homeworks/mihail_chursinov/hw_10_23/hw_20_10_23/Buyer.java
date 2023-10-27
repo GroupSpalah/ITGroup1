c142212344d98563f3cb9e97913efef507d1074f
Mykhailo515 <mikhail.chursinov@gmail.com>
2023-10-27 16:38:17 +0300

    Commit Init

---
+++ src/homeworks/mihail_chursinov/hw_10_23/hw_20_10_23/Buyer.java
@@ -0,0 +1,31 @@
+package homeworks.mihail_chursinov.hw_10_23.hw_20_10_23;
+
+import java.util.Set;
+
+public class Buyer implements Runnable{
+    private String desiredTicket;
+    private Set<String> tickets;
+
+    public Buyer(String desiredTicket, Set<String> tickets) {
+        this.desiredTicket = desiredTicket;
+        this.tickets = tickets;
+    }
+
+    @Override
+    public void run() {
+        synchronized (tickets) {
+            if (tickets.contains(desiredTicket)) {
+                System.out.println("Ticket " + desiredTicket + " purchased " + Thread.currentThread().getName());
+                tickets.remove(desiredTicket);
+            } else {
+                try {
+                    System.out.println("Ticket " + desiredTicket + " not found " + Thread.currentThread().getName()
+                            + " wait");
+                    tickets.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
---
+++ src/homeworks/mihail_chursinov/hw_10_23/hw_20_10_23/CashBox.java
@@ -0,0 +1,70 @@
+package homeworks.mihail_chursinov.hw_10_23.hw_20_10_23;
+
+import java.util.*;
+
+public class  CashBox {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static volatile Set<String> tickets = new LinkedHashSet<>();
+
+    public static void main(String[] args) {
+
+        while (true) {
+            menu();
+            String key = SCANNER.next();
+            try {
+                switch (key) {
+                    case "1": {
+                        String ticket = getInfoFromUser("Please, type name of ticket that you want to sell");
+
+                        String sellerName = getInfoFromUser("Please, type name of seller");
+
+                        Seller seller = new Seller(ticket, tickets);
+
+                        Thread threadSeller = new Thread(seller, sellerName);
+
+                        threadSeller.start();
+                        break;
+                    }
+                    case "2": {
+                        String ticket = getInfoFromUser("Please, type name of ticket that you want to buy");
+
+                        String passerName = getInfoFromUser("Please, type name of buyer");
+
+                        Buyer buyer = new Buyer(ticket, tickets);
+
+                        Thread threadBuyer = new Thread(buyer, passerName);
+
+                        threadBuyer.start();
+                        break;
+                    }
+                    case "3": {
+                        return;
+                    }
+                    default:
+                        return;
+                }
+            } catch (Exception e) {
+                System.out.println("Exception" + e);
+            }
+        }
+
+    }
+
+    private static String getInfoFromUser(String message) {
+        System.out.println(message);
+        return new Scanner(System.in).next();
+    }
+
+    public static void menu() {
+        System.out.println(
+                "1) If you want to sell ticket.\n" +
+                "2) If you want to buy ticket.\n" +
+                "3) Exit.");
+
+        String ticketToAdd = "Kyiv-Lviv";
+        tickets.add(ticketToAdd);
+    }
+}
+
+
+
