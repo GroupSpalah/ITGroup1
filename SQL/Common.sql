23c92dd92e591f57e596af5cb38d4b10ea08eeef
Andrey Valevskiy <valevskii.a@gmail.com>
2023-10-31 18:34:48 +0200

    Constraints, delete, drop, truncate

--- SQL/Common.sql
+++ SQL/Common.sql
@@ -29,3 +29,50 @@ SELECT * FROM person ORDER BY age ASC;
 
 SELECT * FROM person ORDER BY age DESC;
 
+/*Wild cards*/
+SELECT * FROM person WHERE last_name LIKE '%e%';
+
+/*Update*/
+
+UPDATE person SET first_name = 'Ken' WHERE first_name = 'John';
+
+UPDATE person SET age = (age + 5) WHERE first_name = 'Tom';
+
+/*Delete*/
+DELETE FROM person WHERE first_name = 'Ken';
+
+/*Truncate*/
+truncate TABLE  person;
+
+/*Delete table*/
+
+DROP TABLE worker;
+
+/*Sum, Max, Min, Avg*/
+
+SELECT sum(age) FROM person;
+
+SELECT max(age) FROM person;
+SELECT min(age) FROM person;
+SELECT avg(age) FROM person;
+
+/*Primary key*/
+CREATE TABLE Worker(
+worker_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+age INT DEFAULT 15,
+CHECK(age > 10),
+UNIQUE KEY(first_name, last_name));
+
+INSERT INTO worker(first_name, last_name, age)
+VALUES
+('Ann', 'Hanks',    19),
+('Ben', 'Travolta', 78),
+('Ann', 'Hanks',    89);
+
+SELECT * FROM worker;
+
+DELETE FROM worker WHERE worker_id = 3;
+
+Truncate TABLE worker;
--- src/homeworks/anton_gvozdenko/SQL/Script.sql
+++ src/homeworks/anton_gvozdenko/SQL/Script.sql
@@ -1,19 +1,29 @@
 CREATE DATABASE school_sql;
+
 USE school_sql;
- CREATE TABLE student(
+
+CREATE TABLE student(
 sur_name VARCHAR(32),
 first_name VARCHAR(32),
 second_name VARCHAR(32),
 age INT,
 number_school INT);
+
 INSERT INTO student (first_name,sur_name,second_name,age,number_school)
 VALUES 
-('Ivan','Ivanovsch','Ivanov',25,1),
-('Stepan','Stepanovich','Stepanov',9,10),
-('Petr','Petrovich','Petrov',11,7);
-SELECT *FROM student;
-SELECT first_name,sur_name,second_name FROM student;
+('Ivan',   'Ivanovsch',   'Ivanov',  25, 1),
+('Stepan', 'Stepanovich', 'Stepanov', 9, 10),
+('Petr',   'Petrovich',   'Petrov',  11, 7);
+
+SELECT * FROM student;
+
+SELECT first_name, sur_name, second_name
+FROM student;
+
 SELECT * FROM student WHERE age > 10;
+
 SELECT * FROM student WHERE number_school = 7 OR number_school = 10;
-SELECT *FROM student ORDER BY  number_school ASC;
-SELECT *FROM student ORDER BY  number_school DESC;
+
+SELECT * FROM student ORDER BY  number_school;
+
+SELECT * FROM student ORDER BY  number_school DESC;
