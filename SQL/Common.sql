0af97e2af4f2f06a135ca51c346ac9cdcc44a34c
Andrey Valevskiy <valevskii.a@gmail.com>
2023-11-03 18:03:53 +0200

    Inner join, dates, string, date functions

--- SQL/Common.sql
+++ SQL/Common.sql
@@ -9,9 +9,9 @@ age INT);
 
 INSERT INTO person(first_name, last_name, age)
 VALUES
-('John', 'Kennedi', 30),
-('Ben', 'Squirell', 28),
-('Tom', 'Hanks', 31);
+('Barbara', 'Kennedi', 30),
+('Alice', 'Squirell', 28),
+('Izabella', 'Hanks', 31);
 
 SELECT * FROM person;
 
@@ -76,3 +76,76 @@ SELECT * FROM worker;
 DELETE FROM worker WHERE worker_id = 3;
 
 Truncate TABLE worker;
+
+/* LIMIT, OFFSET */
+SELECT * FROM person LIMIT 2 OFFSET 2;
+
+/*INNER JOIN*/
+
+CREATE TABLE Human(
+human_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+age INT, 
+FK_Human_Address INT,
+FOREIGN KEY (FK_Human_Address) REFERENCES Address(address_id));
+
+CREATE TABLE Address(
+address_id INT PRIMARY KEY AUTO_INCREMENT,
+country ENUM('Ukraine', 'Canada', 'Australia'),
+city VARCHAR(30),
+street VARCHAR(30),
+house_number INT);
+
+INSERT INTO Address(country, city, street, house_number)
+VALUES
+('Ukraine',    'Kiev',   'Schevchenko', 19),
+('Canada',     'Odessa', 'Kievskaya', 20),
+('Australia',  'Lviv',   'Pushkina', 15),
+('Australia',  'Kharkov',   'Zvonkaya', 15);
+
+INSERT INTO human(first_name, last_name, age)
+VALUES
+('Ann', 'Hanks3',    19),
+('Ben', 'Travolta', 78),
+('Ann', 'Hanks2',    89),
+('John', 'Hanks1',    89);
+
+SELECT * FROM human 
+INNER JOIN address 
+ON human.FK_Human_Address = address.address_id;
+
+SELECT h.first_name, h.last_name, a.country, a.city 
+FROM human h
+INNER JOIN address a
+ON h.FK_Human_Address = a.address_id;
+
+SELECT h.first_name, h.last_name, a.country, a.city 
+FROM human h
+INNER JOIN address a
+ON h.FK_Human_Address = a.address_id
+WHERE a.city = 'Kiev';
+
+/*Dates*/
+
+SELECT now();
+SELECT curdate();
+
+SELECT date_add(now(), INTERVAL 2 YEAR); 
+SELECT date_add(now(), INTERVAL 2 HOUR); 
+
+SELECT date_format(now(), '%b %d %Y %h:%i %p'); 
+
+SELECT date_format(now(), '%d %b %y'); 
+
+/*Between*/
+
+SELECT * FROM human h WHERE h.age > 10 AND h.age < 70;
+
+SELECT * FROM human h WHERE h.age BETWEEN 10 AND 90;
+
+/*String function*/
+
+SELECT CONCAT('My', 'S', 'QL');
+
+SELECT CONCAT(h.first_name , ' ', h.last_name) FROM human h;
