2bb03a516fcf498a68657eea56ff605307bc18b6
Andrey Valevskiy <valevskii.a@gmail.com>
2023-11-07 18:28:02 +0200

    Inner join for 3 tables, alter, grouping, string functions

--- SQL/Common.sql
+++ SQL/Common.sql
@@ -2,6 +2,8 @@ CREATE DATABASE learn_sql;
 
 USE learn_sql;
 
+CREATE TEMPORARY TABLE tp_table();
+
 CREATE TABLE Person(
 first_name VARCHAR(30),
 last_name VARCHAR(40),
@@ -88,7 +90,7 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 age INT, 
 FK_Human_Address INT,
-FOREIGN KEY (FK_Human_Address) REFERENCES Address(address_id));
+FOREIGN KEY (FK_Human_Address) REFERENCES AddressNew(address_id));
 
 CREATE TABLE Address(
 address_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -97,6 +99,25 @@ city VARCHAR(30),
 street VARCHAR(30),
 house_number INT);
 
+CREATE TABLE AddressNew(
+address_id INT PRIMARY KEY AUTO_INCREMENT,
+FK_Address_Country INT NULL,
+city VARCHAR(30),
+street VARCHAR(30),
+house_number INT,
+FOREIGN KEY (FK_Address_Country) REFERENCES Country(country_id));
+
+CREATE TABLE Country(
+country_id INT PRIMARY KEY AUTO_INCREMENT,
+name ENUM('Ukraine', 'Canada', 'Australia'));
+
+INSERT INTO Country(name)
+VALUES
+('Ukraine'),
+('Canada'),
+('Australia'),
+('Australia');
+
 INSERT INTO Address(country, city, street, house_number)
 VALUES
 ('Ukraine',    'Kiev',   'Schevchenko', 19),
@@ -104,12 +125,27 @@ VALUES
 ('Australia',  'Lviv',   'Pushkina', 15),
 ('Australia',  'Kharkov',   'Zvonkaya', 15);
 
-INSERT INTO human(first_name, last_name, age)
+INSERT INTO AddressNew(city, street, house_number, FK_Address_Country)
 VALUES
-('Ann', 'Hanks3',    19),
-('Ben', 'Travolta', 78),
-('Ann', 'Hanks2',    89),
-('John', 'Hanks1',    89);
+('Kiev',   'Schevchenko', 19, 1),
+('Odessa', 'Kievskaya', 20, 2),
+('Lviv',   'Pushkina', 15, 3),
+('Kharkov',   'Zvonkaya', 15, 1);
+
+INSERT INTO human(first_name, last_name, age, FK_Human_Address)
+VALUES
+('Ann', 'Hanks3',    19, 1),
+('Ben', 'Travolta', 78, 1),
+('Ann', 'Hanks2',    89, 2),
+('John', 'Hanks1',    89, 2);
+
+SELECT * 
+FROM human h
+INNER JOIN addressnew an
+ON h.FK_Human_Address = an.address_id
+INNER JOIN country c 
+ON c.country_id = an.FK_Address_Country;
+
 
 SELECT * FROM human 
 INNER JOIN address 
@@ -149,3 +185,67 @@ SELECT * FROM human h WHERE h.age BETWEEN 10 AND 90;
 SELECT CONCAT('My', 'S', 'QL');
 
 SELECT CONCAT(h.first_name , ' ', h.last_name) FROM human h;
+
+/*Grouping*/
+
+SELECT h.FK_Human_Address, COUNT(*)
+FROM human h 
+GROUP BY h.FK_Human_Address;
+
+SELECT h.FK_Human_Address, COUNT(*)
+FROM human h 
+GROUP BY h.FK_Human_Address
+HAVING count(*) > 1;
+
+SELECT h.FK_Human_Address, COUNT(*)
+FROM human h 
+GROUP BY h.FK_Human_Address
+ORDER BY COUNT(*) DESC;
+
+SELECT h.FK_Human_Address, COUNT(*)
+FROM human h 
+WHERE 1 = 1
+GROUP BY h.FK_Human_Address
+HAVING count(*) > 1;
+
+SELECT a.country, COUNT(*)
+FROM human h 
+INNER JOIN address a 
+ON h.FK_Human_Address = a.address_id
+GROUP BY a.country;
+
+/*Alter*/
+
+CREATE TABLE Example(
+example_id INT PRIMARY KEY AUTO_INCREMENT,
+ex_number INT);
+
+INSERT INTO example(ex_number)
+VALUES
+(10),
+(15);
+
+ALTER TABLE example 
+ADD city VARCHAR(30) DEFAULT 'Lviv';
+
+ALTER TABLE example 
+MODIFY COLUMN city VARCHAR(100);
+
+/*String function task*/
+/*
+ * В таблице есть столбец с именем, некоторые буквы в верхнем и нижнем регистрах.
+Изменить имена так, чтобы первая буква была в верхнем регистре, а остальные в нижнем.
+ * */
+SELECT substring('RelATIonSHips' FROM 2);
+
+SELECT substring('Relationships', 1, 1);
+
+SELECT substring('Relationships' FROM 2);
+
+SELECT upper('Hello');
+
+SELECT lower('Hello');
+
+SELECT concat(upper(substring('RelATIonSHips', 1, 1)), lower(substring('RelATIonSHips' FROM 2)));
+
+UPDATE example SET city = concat(upper(substring(city, 1, 1)), lower(substring(city FROM 2)));
